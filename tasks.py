7d9eba957aa0fd77d7864c582914b82c95ab0b5f
ryanmerolle <ryan.merolle@gmail.com>
2023-11-24 17:30:55 +0000

    add clab to devcontainer

---
+++ tasks.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+from invoke import task
+import subprocess
+
+
+@task()
+def devcontainer_setup(ctx):
+    """
+    Set up the development container environment.
+    This task runs other tasks to install various requirements.
+    """
+    # Install Python project and development requirements
+    install_python_reqs(ctx)
+    install_python_reqs_dev(ctx)
+
+    # Install Ansible collection requirements
+    install_ansible_collections(ctx)
+
+    # Install ContainerLab
+    install_clab(ctx)
+
+
+@task()
+def install_python_reqs(ctx):
+    """
+    Install project Python requirements.
+    """
+    print("Installing Project Python Requirements")
+    ctx.run("pip install --upgrade pip")
+    ctx.run("pip install -r requirements.txt")
+
+
+@task()
+def install_python_reqs_dev(ctx):
+    """
+    Install development Python requirements.
+    """
+    print("Installing Development Python Requirements")
+    ctx.run("grep -v 'invoke' requirements-dev.txt | xargs pip install")
+
+
+@task()
+def install_ansible_collections(ctx):
+    """
+    Install Ansible collection requirements, including Arista AVD collection.
+    """
+    print("Installing Project Ansible Collection Requirements")
+    ctx.run("ansible-galaxy collection install -r requirements.yml --force-with-deps")
+
+    print("Installing Project Arista AVD Collection Python Requirements")
+
+    # Run the command and capture its output
+    result = subprocess.run(
+        "ansible-galaxy collection list arista.avd --format yaml | head -1 | cut -d: -f1",
+        shell=True,
+        text=True,
+        capture_output=True,
+    )
+
+    if result.returncode == 0:
+        # Extract the output and remove any trailing newlines
+        arista_avd_dir = result.stdout.strip()
+        ctx.run(f"pip install -r {arista_avd_dir}/arista/avd/requirements.txt")
+    else:
+        print("Error obtaining ARISTA_AVD_DIR:", result.stderr)
+
+
+@task()
+def install_clab(ctx):
+    """
+    Install ContainerLab specifically for the devcontainer environment.
+    """
+    print("Installing ContainerLab")
+    ctx.run(
+        "echo 'deb [trusted=yes] https://apt.fury.io/netdevops/ /' | sudo tee -a /etc/apt/sources.list.d/netdevops.list"
+    )
+    ctx.run("sudo apt update && sudo apt install containerlab")
+
+
+@task()
+def clab_deploy(ctx):
+    """
+    Create ContainerLab Topology.
+    """
+    print("Deploying ContainerLab Topology")
+    ctx.run("sudo -S clab deploy -t lab.yml --reconfigure")
+
+
+@task()
+def clab_destroy(ctx):
+    """
+    Destroy ContainerLab Topology.
+    """
+    print("Destroying ContainerLab Topology")
+    ctx.run("sudo -S clab destroy -t lab.yml")
+
+
+@task()
+def download_eos(ctx):
+    """
+    Download EOS.
+    """
+    print("Downloading EOS image")
+    ctx.run("ardl get eos --version 4.30.2F --image-type cEOS --import-docker")
+
+
+@task()
+def clear_ansible_log(ctx):
+    """
+    Clear ansible.log file.
+    """
+    print("Clearing ansible log file")
+    open('ansible.log', 'w', encoding="utf-8").close()
